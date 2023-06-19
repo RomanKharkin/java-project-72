664bc4616e72a2bd04fa63558200bec41752b8bf
Roman Kharkin <rv.h@bk.ru>
2023-06-19 21:54:32 +0200

    Шаг4: Подключил шаблонизатор Thymeleaf.

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,12 +1,21 @@
 package hexlet.code;
 
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
 
 public class App {
 
     public static Javalin getApp() {
         Javalin app = Javalin.create(config -> {
             config.plugins.enableDevLogging();
+            // Подключаем настроенный шаблонизатор к фреймворку
+            JavalinThymeleaf.init(getTemplateEngine());
         });
 
         addRoutes(app);
@@ -21,7 +30,24 @@ public class App {
     }
 
     private static void addRoutes(Javalin app) {
-        app.get("/", ctx -> ctx.result("Hello World"));
+        app.get("/", ctx -> ctx.render("index.html"));
+    }
+
+    // Javalin поддерживает работу с шаблонизатором thymeleaf
+    private static TemplateEngine getTemplateEngine() {
+        // Создаём инстанс движка шаблонизатора
+        TemplateEngine templateEngine = new TemplateEngine();
+        // Добавляем к нему диалекты
+        templateEngine.addDialect(new LayoutDialect());
+        templateEngine.addDialect(new Java8TimeDialect());
+        // Настраиваем преобразователь шаблонов, так, чтобы обрабатывались
+        // шаблоны в директории /templates/
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/templates/");
+        // Добавляем преобразователь шаблонов к движку шаблонизатора
+        templateEngine.addTemplateResolver(templateResolver);
+
+        return templateEngine;
     }
 
 }
